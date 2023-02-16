8913cb29c32f8ef2f5c58d6a69fc17afb6991468
LaoWan <1970558400@qq.com>
2023-02-16 09:37:47 +0800

    echarts-util
    下载了echarts-util依赖

--- MyappServer/index.js
+++ MyappServer/index.js
@@ -1,5 +1,6 @@
 let express=require("express");
 let app=express();
+
 // 设置跨域
 app.use(function(req,res,next){
     res.header('Access-Control-Allow-Origin', '*');
@@ -10,14 +11,14 @@ app.use(function(req,res,next){
     // 千万不要网%%￥￥￥###
     next();
 })
- 
+
 // 引用路由文件
 let chartOne=require("./router/one");
 let chartTwo=require("./router/two");
 let chartThree=require("./router/three");
 let chartFour=require("./router/four");
 let chartMap=require("./router/map");
- 
+
 // 使用中间件来配置路由
 app.use("/one",chartOne)
 app.use("/two",chartTwo)
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <script>
-import {provide} from 'vue'
-import * as echarts from 'echarts'
-import axios from 'axios'
+import { provide } from "vue";
+import * as echarts from "echarts";
+import axios from "axios";
 
 // 设置基准路径
-axios.defaults.baseURL="http://localhost:8888"
+axios.defaults.baseURL = "http://localhost:8888";
 
 export default {
   setup() {
     // provider("另起名称","传递的内容")
-    provide("echarts",echarts)
-    provide("axios",axios)
-  }
-}
+    provide("echarts", echarts);
+    provide("axios", axios);
+  },
+};
 </script>
 
 <style lang="less">
-body { 
-  background: url('~@/assets/bg.jpg') top center no-repeat;
+body {
+  background: url("~@/assets/bg.jpg") top center no-repeat;
 }
 
 * {
@@ -29,5 +29,4 @@ body {
   padding: 0;
   box-sizing: border-box;
 }
-
 </style>
